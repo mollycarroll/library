250a17a79c50bf4c6eb6b8b84b325dc16a14b5e8
Molly Geerling <mollygeerling@gmail.com>
2020-01-02 20:21:59 -0700

    Update revised code in html and js from earlier work, add style sheet

--- library.js
+++ library.js
@@ -1,15 +1,32 @@
+function showForm() {
+  document.getElementById('newBookForm').style.display = "block";
+}
+
 let myLibrary = [];
 
-function Book(title, author, pages, status) {
+function Book() {
   this.title = title
   this.author = author
   this.pages = pages
   this.status = status
-  this.info = function() {
-    console.log(title + " by " + author + ", " + status)
-  }
 }
 
 function addBookToLibrary() {
+    newBook = new Book(title, author, pages, status)
+    myLibrary.push(newBook)
+    render(newBook)
+  }
 
-}
+  var titleText = document.createTextNode(document.getElementById("title").value);
+  var authorText = document.createTextNode(document.getElementById("author").value);
+  var pagesText = document.createTextNode(document.getElementById("pages").value);
+
+  }
+
+  function render() {
+    var newRow = HTMLTableElement.insertRow(-1);
+    var newTitle = HTMLTableRowElement.insertCell(0);
+    var newAuthor = HTMLTableRowElement.insertCell(1);
+    var newPages = HTMLTableRowElement.insertCell(2);
+    var newStatus = HTMLTableRowElement.insertCell(3);
+  }
