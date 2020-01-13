b880fb0a4dda3f488d7743942c1d1c8d7ca15d92
Molly Geerling <mollygeerling@gmail.com>
2020-01-13 22:27:51 +0000

    create book constructor, add book function, submit button event listener, add attributes to necessary html elements

--- library.js
+++ library.js
@@ -1,2 +1,34 @@
 let myLibrary = [];
 
+const libraryTable = document.querySelector("htmlLibraryTable");
+
+function Book(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+function addBookToLibrary() {
+
+    let newTitle = document.querySelector("#title").value;
+    let newAuthor = document.querySelector("#author").value;
+    let newPages = document.querySelector("#pages").value;
+    let newStatus = document.querySelector("#status").value;
+
+    let newBook = new Book(newTitle, newAuthor, newPages, newStatus);
+
+    let row = table.insertRow(1);
+    
+    for (c = 0; c < 5; c++) {
+        let cell = row.insertCell(c);
+    }
+
+    cell[0].innerHTML = newTitle;
+    cell[1].innerHTML = newAuthor;
+    cell[2].innerHTML = newPages;
+    cell[3].innerHTML = newStatus;
+    
+}
+
+const submitButton = document.querySelector("#submit").addEventListener("click", addBookToLibrary);
